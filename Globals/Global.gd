62c4d0ce6606cd58be380f4eabed0d5846f5c332
schmertenk <merten-koch1@t-online.de>
2024-05-10 22:46:02 +0200

    changed save file path and create one on first play

--- Globals/Global.gd
+++ Globals/Global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SAVE_FILE_PATH = "user://save.json"
+
 var main:Main
 var level:Level
 
@@ -7,10 +9,12 @@ var coins = 0
 var points = 0
 
 func _ready():
-	_load("./save.json")
+	if ! FileAccess.file_exists(SAVE_FILE_PATH):
+		_save(SAVE_FILE_PATH)
+	_load(SAVE_FILE_PATH)
 
 func _exit_tree():
-	_save("./save.json")
+	_save(SAVE_FILE_PATH)
 
 func _save(path):
 	var data = {
